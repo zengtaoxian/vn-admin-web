27930593d8a81ffa07aaeb5d9879faae0ac44900
zengtaoxian <zengtaoxian@163.com>
2019-10-12 11:31:58 +0800

    修改登陆title

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
   <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on"
              label-position="left">
-      <h3 class="title">语音通知管理平台</h3>
+      <h3 class="title">系统登陆</h3>
       <el-form-item prop="name">
         <span class="svg-container">
           <svg-icon icon-class="user"/>
