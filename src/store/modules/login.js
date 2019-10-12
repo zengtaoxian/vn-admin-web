1ac21bb36fa46f441237c519096f71cc7121a062
zengtaoxian <zengtaoxian@163.com>
2019-10-12 11:30:04 +0800

    完成登出操作

--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -1,24 +1,24 @@
-import { login, logout } from '@/api/login'
+import {login, logout} from '@/api/login'
 import Cookies from 'js-cookie'
 
 //consts
-const LOGIN_TOKEN = 'LOGIN_TOKEN'
-const LOGIN_NAME = 'LOGIN_NAME'
-const LOGIN_UID = 'LOGIN_UID'
+const LOGIN_TOKEN = 'LOGIN_TOKEN';
+const LOGIN_NAME = 'LOGIN_NAME';
+const LOGIN_UID = 'LOGIN_UID';
 
 //states
 const state = {
   token: Cookies.get(LOGIN_TOKEN) || '',
   name: Cookies.get(LOGIN_NAME) || '',
   uid: Cookies.get(LOGIN_UID) || ''
-}
+};
 
 //getters
 const getters = {
   token: state => state.token,
   name: state => state.name,
   uid: state => state.uid
-}
+};
 
 //mutations
 const mutations = {
@@ -31,21 +31,21 @@ const mutations = {
   LOGIN_UID: (state, uid) => {
     state.uid = uid
   }
-}
+};
 
 //actions
 const actions = {
   //登录
-  Login({ commit }, userInfo) {
+  Login({commit}, userInfo) {
     return new Promise((resolve, reject) => {
       login(userInfo).then(response => {
-        const data = response.data
-        Cookies.set(LOGIN_TOKEN, data.token)
-        Cookies.set(LOGIN_NAME, userInfo.name)
-        Cookies.set(LOGIN_UID, data.user.uid)
-        commit(LOGIN_TOKEN, data.token)
-        commit(LOGIN_NAME, userInfo.name)
-        commit(LOGIN_UID, data.user.uid)
+        const data = response.data;
+        Cookies.set(LOGIN_TOKEN, data.token);
+        Cookies.set(LOGIN_NAME, userInfo.name);
+        Cookies.set(LOGIN_UID, data.user.uid);
+        commit(LOGIN_TOKEN, data.token);
+        commit(LOGIN_NAME, userInfo.name);
+        commit(LOGIN_UID, data.user.uid);
         resolve()
       }).catch(error => {
         reject(error)
@@ -54,14 +54,14 @@ const actions = {
   },
 
   //登出
-  LogOut({ commit, state }) {
+  LogOut({commit, state}) {
     return new Promise((resolve, reject) => {
       logout(state.token).then(() => {
-        commit(LOGIN_TOKEN, '')
-        commit(LOGIN_NAME, '')
-        commit(LOGIN_UID, '')
-        Cookies.remove(LOGIN_TOKEN)
-        Cookies.remove(LOGIN_UID)
+        commit(LOGIN_TOKEN, '');
+        commit(LOGIN_UID, '');
+        Cookies.remove(LOGIN_TOKEN);
+        Cookies.remove(LOGIN_NAME);
+        Cookies.remove(LOGIN_UID);
         resolve()
       }).catch(error => {
         reject(error)
@@ -70,16 +70,17 @@ const actions = {
   },
 
   // 前端登出
-  FedLogOut({ commit }) {
+  FedLogOut({commit}) {
     return new Promise(resolve => {
-      commit(LOGIN_TOKEN, '')
-      commit(LOGIN_UID, '')
-      Cookies.remove(LOGIN_TOKEN)
-      Cookies.remove(LOGIN_UID)
+      commit(LOGIN_TOKEN, '');
+      commit(LOGIN_UID, '');
+      Cookies.remove(LOGIN_TOKEN);
+      Cookies.remove(LOGIN_NAME);
+      Cookies.remove(LOGIN_UID);
       resolve()
     })
   }
-}
+};
 
 export default {
   namespaced: true,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on"
+             label-position="left">
       <h3 class="title">语音通知管理平台</h3>
       <el-form-item prop="name">
         <span class="svg-container">
-          <svg-icon icon-class="user" />
+          <svg-icon icon-class="user"/>
         </span>
-        <el-input v-model="loginForm.name" name="username" type="text" auto-complete="on" placeholder="username" />
+        <el-input v-model="loginForm.name" name="username" type="text" auto-complete="on" placeholder="username"/>
       </el-form-item>
       <el-form-item prop="password">
         <span class="svg-container">
-          <svg-icon icon-class="password" />
+          <svg-icon icon-class="password"/>
         </span>
         <el-input
           :type="pwdType"
@@ -18,9 +19,9 @@
           name="password"
           auto-complete="on"
           placeholder="password"
-          @keyup.enter.native="handleLogin" />
+          @keyup.enter.native="handleLogin"/>
         <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="pwdType === 'password' ? 'eye' : 'eye-open'" />
+          <svg-icon :icon-class="pwdType === 'password' ? 'eye' : 'eye-open'"/>
         </span>
       </el-form-item>
       <el-form-item>
@@ -33,157 +34,170 @@
 </template>
 
 <script>
+    import {mapGetters} from "vuex"
 
-export default {
-  name: 'Login',
-  data() {
-    const validatePass = (rule, value, callback) => {
-      if (value.length < 5) {
-        callback(new Error('密码不能小于5位'))
-      } else {
-        callback()
-      }
-    }
-    return {
-      loginForm: {
-        name: 'admin',
-        password: 'admin'
-      },
-      loginRules: {
-        name: [{ required: true, trigger: 'blur' }],
-        password: [{ required: true, trigger: 'blur', validator: validatePass }]
-      },
-      loading: false,
-      pwdType: 'password',
-      redirect: undefined
-    }
-  },
-  watch: {
-    $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect
-      },
-      immediate: true
-    }
-  },
-  methods: {
-    showPwd() {
-      if (this.pwdType === 'password') {
-        this.pwdType = ''
-      } else {
-        this.pwdType = 'password'
-      }
-    },
-    handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('login/Login', this.loginForm).then(() => {
-            this.loading = false
-            this.$router.push({ path: this.redirect || '/' })
-          }).catch(() => {
-            this.loading = false
-            this.$message({
-                message: '用户名或密码不正确!',
-                type: 'error',
-                duration: 2 * 1000
-            })
-          })
-        } else {
-          this.$message('error submit!!')
-          return false
+    export default {
+        name: 'Login',
+        data() {
+            const validatePass = (rule, value, callback) => {
+                if (value.length < 5) {
+                    callback(new Error('密码不能小于5位'))
+                } else {
+                    callback()
+                }
+            };
+            return {
+                loginForm: {
+                    name: this.name,
+                    password: ''
+                },
+                loginRules: {
+                    name: [{required: true, trigger: 'blur'}],
+                    password: [{required: true, trigger: 'blur', validator: validatePass}]
+                },
+                loading: false,
+                pwdType: 'password',
+                redirect: undefined
+            }
+        },
+        computed: mapGetters('login', {
+            name: "name"
+        }),
+        watch: {
+            $route: {
+                handler: function (route) {
+                    this.redirect = route.query && route.query.redirect
+                },
+                immediate: true
+            }
+        },
+        methods: {
+            showPwd() {
+                if (this.pwdType === 'password') {
+                    this.pwdType = ''
+                } else {
+                    this.pwdType = 'password'
+                }
+            },
+            handleLogin() {
+                this.$refs.loginForm.validate(valid => {
+                    if (valid) {
+                        this.loading = true;
+                        this.$store.dispatch('login/Login', this.loginForm).then(() => {
+                            this.loading = false;
+                            this.$router.push({path: this.redirect || '/'})
+                        }).catch(() => {
+                            this.loading = false;
+                            this.$message({
+                                message: '用户名或密码不正确!',
+                                type: 'error',
+                                duration: 2 * 1000
+                            })
+                        })
+                    } else {
+                        this.$message('error submit!!');
+                        return false
+                    }
+                })
+            }
         }
-      })
     }
-  }
-}
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-$bg:#2d3a4b;
-$light_gray:#eee;
+  $bg: #2d3a4b;
+  $light_gray: #eee;
 
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
+  /* reset element-ui css */
+  .login-container {
+    .el-input {
+      display: inline-block;
       height: 47px;
-      &:-webkit-autofill {
-        -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: #fff !important;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+
+        &:-webkit-autofill {
+          -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
+          -webkit-text-fill-color: #fff !important;
+        }
       }
     }
+
+    .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
   }
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-}
 
 </style>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
-.login-container {
-  position: fixed;
-  height: 100%;
-  width: 100%;
-  background-color: $bg;
-  .login-form {
-    position: absolute;
-    left: 0;
-    right: 0;
-    width: 520px;
-    max-width: 100%;
-    padding: 35px 35px 15px 35px;
-    margin: 120px auto;
-  }
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-    span {
-      &:first-of-type {
-        margin-right: 16px;
+  $bg: #2d3a4b;
+  $dark_gray: #889aa4;
+  $light_gray: #eee;
+  .login-container {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    background-color: $bg;
+
+    .login-form {
+      position: absolute;
+      left: 0;
+      right: 0;
+      width: 520px;
+      max-width: 100%;
+      padding: 35px 35px 15px 35px;
+      margin: 120px auto;
+    }
+
+    .tips {
+      font-size: 14px;
+      color: #fff;
+      margin-bottom: 10px;
+
+      span {
+        &:first-of-type {
+          margin-right: 16px;
+        }
       }
     }
+
+    .svg-container {
+      padding: 6px 5px 6px 15px;
+      color: $dark_gray;
+      vertical-align: middle;
+      width: 30px;
+      display: inline-block;
+    }
+
+    .title {
+      font-size: 26px;
+      font-weight: 400;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+
+    .show-pwd {
+      position: absolute;
+      right: 10px;
+      top: 7px;
+      font-size: 16px;
+      color: $dark_gray;
+      cursor: pointer;
+      user-select: none;
+    }
   }
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-  .title {
-    font-size: 26px;
-    font-weight: 400;
-    color: $light_gray;
-    margin: 0px auto 40px auto;
-    text-align: center;
-    font-weight: bold;
-  }
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-}
 </style>
